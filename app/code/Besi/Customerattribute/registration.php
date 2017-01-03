9f7afb159410da31e80c8173f8ca7edf6d68d558
besi95 <saraci.besim@gmail.com>
2017-01-03 18:58:36 +0100

    updatimi i versionit te magentos

--- app/code/Besi/Customerattribute/registration.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Besi_Customerattribute',
-    __DIR__
-);
--- app/code/FishPig/WordPress/registration.php
+++
@@ -1,5 +0,0 @@
-<?php
-	
-	use \Magento\Framework\Component\ComponentRegistrar;
-
-	ComponentRegistrar::register(ComponentRegistrar::MODULE, 'FishPig_WordPress', __DIR__);
